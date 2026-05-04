d6ebdded9b3dcce4955bd9f5a7504c75c2a76ae9
avantika6838-tech <avantika.6838@gmail.com>
2026-05-04 17:11:39 +0530

    Que1.c
    This program performs the addition of two matrices of the same dimensions. It takes input for the dimensions and elements of both matrices, then outputs the resultant matrix after addition.

---
+++ Day 11/Que1.c
@@ -0,0 +1,133 @@
+/* 
+Problem: Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.
+
+Input:
+- First line: two integers m and n representing the number of rows and columns
+- Next m lines: n integers each representing the elements of the first matrix
+- Next m lines: n integers each representing the elements of the second matrix
+
+Output:
+- Print the resultant matrix after addition, with each row on a new line and elements separated by spaces
+
+Example:
+Input:
+3 3
+1 2 3
+4 5 6
+7 8 9
+9 8 7
+6 5 4
+3 2 1
+
+Output:
+10 10 10
+10 10 10
+10 10 10
+
+Explanation:
+Each element of the result matrix is the sum of the corresponding elements from the two input matrices.
+
+Test Cases:
+
+Test Case 1:
+Input:
+2 2
+1 2
+3 4
+5 6
+7 8
+Output:
+6 8
+10 12
+
+Test Case 2:
+Input:
+1 3
+10 20 30
+1 2 3
+Output:
+11 22 33
+
+Test Case 3:
+Input:
+3 1
+1
+2
+3
+4
+5
+6
+Output:
+5
+7
+9
+*/
+
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+    int A[100][100];    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    int B[100][100];
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(j > 0) printf(" ");
+            printf("%d", A[i][j] + B[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+
+/*
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+
+
+ 
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+*/
+
+int** transpose(int** matrix, int matrixSize, const int* matrixColSize, int* returnSize, int** returnColumnSizes) {
+    int m = matrixSize;           
+    int n = matrixColSize[0];    
+    *returnSize = n;
+    *returnColumnSizes = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        (*returnColumnSizes)[i] = m;
+    }
+    
+    int** result = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        result[i] = (int*)malloc(m * sizeof(int));
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            result[j][i] = matrix[i][j];
+        }
+    }
+    
+    return result;
+}
